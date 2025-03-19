e7e41307b3f17ef1fb2694134502437cf32c3c64
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-03-19 19:12:45 +0000

    Add Rhombus

--- src/griffith/Rhombus.java
+++ src/griffith/Rhombus.java
@@ -1,7 +1,11 @@
 package griffith;
 
 public class Rhombus extends Shape {
-
+	
+	private double side;
+	private double diagonal_1;
+	private double diagonal_2;
+	
 	@Override
 	double area() {
 		// TODO Auto-generated method stub
@@ -13,5 +17,13 @@ public class Rhombus extends Shape {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public String toString() {
+		return "Rhombus side=" + side + ", diagonal_1=" + diagonal_1 + ", diagonal_2=" + diagonal_2 + ", Name="
+				+ getName();
+	}
+	
+	
 
 }
