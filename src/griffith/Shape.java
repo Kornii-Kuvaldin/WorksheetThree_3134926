5eaed2ad9ff2e4057f1f876e68f6b8bade0c7408
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-03-19 18:57:23 +0000

    Add constructor, getters and setters, abstract methods, toString

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -1,5 +1,30 @@
 package griffith;
 
 public abstract class Shape {
+	private String name;
+
+
+
+	public Shape(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	abstract double area();
+
+	abstract double perimeter();
+
+	@Override
+	public String toString() {
+		return name;
+	}
 
 }
