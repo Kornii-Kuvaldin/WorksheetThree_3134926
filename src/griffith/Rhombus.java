d5cd266e6a63fdf3f715e1502675e4c89f611ba9
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-03-19 19:00:35 +0000

    Add subclasses

---
+++ src/griffith/Rhombus.java
@@ -0,0 +1,17 @@
+package griffith;
+
+public class Rhombus extends Shape {
+
+	@Override
+	double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	double perimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
